7fc1b6563dda8e4beee700a58c8a11a9c122c700
Tom Chen <chen.thomas.c@gmail.com>
2021-08-10 00:33:06 -0400

    add missing semicolon

--- app/controllers/receivers.js
+++ app/controllers/receivers.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 
 export default class ReceiversController extends Controller {
-  queryParams = ['state']
+  queryParams = ['state'];
 
   state = null;
 }
