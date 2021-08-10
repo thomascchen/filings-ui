0cc707915279ff9def23e5039b495e72212f74f0
Tom Chen <chen.thomas.c@gmail.com>
2021-08-09 23:23:33 -0400

    add adapter and specify host

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.HOST = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
@@ -45,6 +46,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.HOST = 'https://fathomless-beach-32422.herokuapp.com';
   }
 
   return ENV;
